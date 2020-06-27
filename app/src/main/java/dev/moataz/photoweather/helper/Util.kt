8d6a6f19157492897388686da7efabf6416eba29
moataz <mtzhisham@gmail.com>
2020-06-27 05:56:46 +0200

    [fix] fix merge conflicts

---
+++ app/src/main/java/dev/moataz/photoweather/helper/Util.kt
@@ -0,0 +1,18 @@
+package dev.moataz.photoweather.helper
+
+import android.content.res.Resources
+import androidx.fragment.app.FragmentActivity
+import dev.moataz.photoweather.R
+import java.io.File
+
+    fun getOutputDirectory(fragmentActivity: FragmentActivity, resources: Resources): File {
+
+        val mediaDir = fragmentActivity.externalMediaDirs.firstOrNull()?.let {
+            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+        return if (mediaDir != null && mediaDir.exists())
+            mediaDir else File("")
+    }
+
+    fun listFiles(fragmentActivity: FragmentActivity, resources: Resources) : ArrayList<File>{
+        return getOutputDirectory(fragmentActivity, resources).listFiles()?.toCollection(ArrayList<File>())?:arrayListOf()
+    }
