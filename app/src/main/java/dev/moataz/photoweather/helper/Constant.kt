701a07870b29a5d27213a406ef55d08a85418f43
moataz <mtzhisham@gmail.com>
2020-06-27 15:36:00 +0200

    [resources] moved strings outside [format] reformatted code [fix] general improvements

--- app/src/main/java/dev/moataz/photoweather/helper/Constant.kt
+++ app/src/main/java/dev/moataz/photoweather/helper/Constant.kt
@@ -1,7 +1,34 @@
 package dev.moataz.photoweather.helper
 
+import android.Manifest
+
 object Constant {
     const val BASE_URL = "https://api.openweathermap.org/data/2.5/"
 
+    const val TAG = "PhotoWeather"
+    const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    const val CAMERA_REQUEST_CODE_PERMISSIONS = 10
+    val CAMERA_REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+
+    const val IMAGE_BUNDLE_KEY = "IMAGE_BUNDLE_KEY"
+    const val GALLERY_BUNDLE_KEY = "IMAGE_BUNDLE_KEY"
+
+
+    const val STORAGE_REQUEST_CODE_PERMISSIONS = 11
+    val STORAGE_REQUIRED_PERMISSIONS = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+
+
+    const val LOCATION_REQUEST_CODE_PERMISSIONS = 12
+
+    const val GPS_REQUEST_CODE_PERMISSIONS = 13
+
+    val LOCATION_REQUIRED_PERMISSIONS = arrayOf(
+        Manifest.permission.ACCESS_FINE_LOCATION,
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    )
 
 }
